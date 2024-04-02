badd81e89e1cc63ccee450b00efe16d34e751730
vedant-dehaley <159027985+vedant-dehaley@users.noreply.github.com>
2024-04-02 22:52:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    var left = document.querySelector(".scroll-img");
+    left.scrollBy(450, 0)
+}
+
+function scrollr() {
+    var right = document.querySelector(".scroll-img");
+    right.scrollBy(-450, 0)
+}
